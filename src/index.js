8b9fef30aafe2b534dd0e6abee508a7b126373ef
Yirim Seck <yirimseck@gmail.com>
2021-08-14 23:49:36 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
   <React.StrictMode>
+     <Auth0Provider
+    domain="dev-rw9jrc6l.us.auth0.com"
+    clientId="rLVvmmnsjJk3JPzkBTJTSbpwVUMV0rgg"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
