ed61f57ed0de57daffcef7f16a63da24b54ccf44
Yirim Seck <yirimseck@gmail.com>
2021-08-21 20:29:48 -0700

    JWT completed lab 11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,8 +2,22 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axiox from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+
 
 class MyFavoriteBooks extends React.Component {
+  handleRequest = async() => { 
+    const {getIdTokenClaims} = this.props.auth0;
+    let tokenClaims = await getIdTokenClaims();
+    const jwt = tokenClaims.__raw;
+    console.log(jwt);
+    const config = {
+      headers: { "Authorization": `Bearer ${jwt}` }
+    } 
+
+  }
+  
   render() {
     return(
       <Jumbotron>
@@ -11,6 +25,7 @@ class MyFavoriteBooks extends React.Component {
         <p>
           This is a collection of my favorite books
         </p>
+        <button onClick={this.handleRequest}>Make Request</button>
       </Jumbotron>
     )
   }
--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
 import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+
 
 class Header extends React.Component {
   render() {
@@ -13,6 +16,7 @@ class Header extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {this.props.isAuthenticated ? <LogoutButton /> : <LoginButton />}
       </Navbar>
     );
   }
